6f12bd1549398f6a55bf7984bf54b8270e59dafa
Boris Duko <boris_blr@mail.ru>
2024-05-22 20:49:55 +0200

    add home Component / connect in routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,16 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { TopBarComponent } from './layout/top-bar/top-bar.component';
-import { FooterComponent } from './layout/footer/footer.component';
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterOutlet } from "@angular/router";
+import { TopBarComponent } from "./layout/top-bar/top-bar.component";
+import { FooterComponent } from "./layout/footer/footer.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
-  imports: [CommonModule, RouterOutlet, TopBarComponent, FooterComponent],
+	selector: "app-root",
+	standalone: true,
+	templateUrl: "./app.component.html",
+	styleUrl: "./app.component.css",
+	imports: [CommonModule, RouterOutlet, TopBarComponent, FooterComponent],
 })
 export class AppComponent {
-  title = 'shopping-store';
+	title = "shopping-store";
 }
