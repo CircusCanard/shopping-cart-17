fa6958bbe11134bb4458855e7812824dc393cb95
Boris Duko <boris_blr@mail.ru>
2024-05-22 20:40:55 +0200

    add create footer w/styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { TopBarComponent } from './layout/top-bar/top-bar.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, TopBarComponent],
+  imports: [CommonModule, RouterOutlet, TopBarComponent, FooterComponent],
 })
 export class AppComponent {
   title = 'shopping-store';
