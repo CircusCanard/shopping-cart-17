1624e7a1d83307a512758d0e173aec1885e46b28
Boris Duko <boris_blr@mail.ru>
2024-06-07 18:17:22 +0200

    add 1.11 Truncate pipe

--- src/app/pages/home/components/product-card/product-card.component.ts
+++ src/app/pages/home/components/product-card/product-card.component.ts
@@ -5,11 +5,12 @@ import {
 	Output,
 } from "@angular/core";
 import { Product } from "../../../../data/products.data";
+import { TruncatePipe } from "../../../../pipes/truncate.pipe";
 
 @Component({
 	selector: "app-product-card",
 	standalone: true,
-	imports: [],
+	imports: [TruncatePipe],
 	templateUrl: "./product-card.component.html",
 	styleUrl: "./product-card.component.css",
 })
